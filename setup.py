6aa0d378e6d88fb4599b3b95056a66723de800f2
Scott Greenwood <greenwoodms@ornl.gov>
2018-10-15 11:41:27 -0400

    file for pip install

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+# Python setup file.
+# See http://packages.python.org/an_example_pypi_project/setuptools.html
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="modelicapy",
+    version="0.1",
+    author="Scott Greenwood",
+    author_email="greenwoodms@ornl.gov",
+    description=("A Python library containing a variety of scripts and functions for interaction with Modelica and TRANSFORM"),
+    long_description=read('README.md'),
+    license="UT-Battell Open Source License (Permissive)",
+    keywords="modelica dymola openmodelica mat",
+    url="https://github.com/ORNL-Modelica/ModelicaPy",
+    install_requires=['future'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Development",
+        "Environment :: Console",
+        "License :: Open Source :: UT-Battelle Permissive",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Utilities"
+    ],
+)
