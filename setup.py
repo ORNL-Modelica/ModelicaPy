4f251dbcdcbdbebb5819849542e23b007531609b
Scott Greenwood <greenwoodms@ornl.gov>
2018-10-15 12:57:12 -0400

    no message

--- setup.py
+++ setup.py
@@ -1,31 +1,31 @@
-import os
 import setuptools
+from os import path
+#this_directory = path.abspath(path.dirname(__file__))
 
-# Guides for making this file:
-# http://packages.python.org/an_example_pypi_project/setuptools.html
-# https://marthall.github.io/blog/how-to-package-a-python-app/
-# https://python-packaging.readthedocs.io/en/latest/minimal.html
+#with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+#    long_description = f.read()
+#     long_description=long_description,
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
+    return open(path.join(path.dirname(__file__), fname)).read()
+	
 setuptools.setup(
     name="modelicapy",
-    version="0.1",
+    version="0.3",
     author="Scott Greenwood",
     author_email="greenwoodms@ornl.gov",
     description=("A Python library containing a variety of scripts and functions for interaction with Modelica and TRANSFORM"),
     long_description=read('README.md'),
-    license="UT-Battell Open Source License (Permissive)",
+    long_description_content_type='text/markdown',
+    license="UT-Battelle Open Source License (Permissive)",
     keywords="modelica dymola openmodelica mat",
     url="https://github.com/ORNL-Modelica/ModelicaPy",
     install_requires=['future'],
     packages=setuptools.find_packages(),
     classifiers=[
-        "Development Status :: 5 - Development",
+        "Development Status :: 3 - Alpha",
         "Environment :: Console",
-        "License :: Open Source :: UT-Battelle Permissive",
+        "License :: Other/Proprietary License",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Intended Audience :: End Users/Desktop",
