e6b5af62542e126477479fd755fd0f4c902c70ff
Scott Greenwood <greenwoodms06@gmail.com>
2022-01-18 11:55:11 -0500

    correct issues with raven.settings and add documetation

--- modelicapy/raven/plugin/src/simulateFMU.py
+++ modelicapy/raven/plugin/src/simulateFMU.py
@@ -4,20 +4,39 @@ Created on Mon Jan 17 15:39:41 2022
 
 @author: Scott Greenwood
 
-RAVEN XML node description
+RAVEN XML node description:
 <inputs> - REQUIRED - float - RAVEN default node for defining in-memory input variables
 <outputs> - REQUIRED - float - RAVEN default node for defining in-memory output variables
-
 <settingsFMU> - custom XML node for FMU specific external model
     <filename> - REQUIRED - str - location of FMU relative to current working directory
+    <parameters> - REQUIRED - str - Variables names of the provided <inputs>. This is required to extract the variable values from the raven object. Note: Should be identical to <inputs> on the raven xml side.
+    <outputs> - REQUIRED -  str - Variables names of the provided <outputs>. This is required to extract the variable values from the raven object. Note: Should be identical to <outputs> on the raven xml side.
     <start_time> - OPTIONAL - float - start time for the FMU simulation. If not provided will default to time in FMU modelDescription.xml.
     <stop_time> - OPTIONAL - float - stop time for the FMU simulation. If not provided will default to time in FMU modelDescription.xml.
     <output_interval> - OPTIONAL - float - time step for simulation. If not provided will default to interval in FMU modelDescription.xml or autocalculated..
-    <parameters> - REQUIRED - str - Variables names of the provided <inputs>. This is required to extract the variable values from the raven object. Note: Should be identical to <inputs> on the raven xml side.
-    <outputs> - REQUIRED -  str - Variables names of the provided <outputs>. This is required to extract the variable values from the raven object. Note: Should be identical to <outputs> on the raven xml side.
+
+IMPORTANT:
+    - If reserved variables names ['start_time', 'stop_time', 'output_interval'] are part of the sample space (i.e., in <parameters.), then the value provided will overwrite the pertinent section.
+    For example, the sample XML input below will use the sampled output_interval values instead of the defined value of 0.5.
 
 TODO:
     - ability to handle time dependent input is not currently supported. It is expected RAVEN can handle this though.
+    
+Example XML Input:
+	<Models>
+		<ExternalModel ModuleToLoad="../../src/simulateFMU" name="simulateFMU" subType="">
+			<inputs>a, b, c, output_interval</inputs>
+			<outputs>x, y, z</outputs>
+			<settingsFMU>
+				<filename>../fmus/myFMU.fmu</filename>
+				<parameters>a, b, c, output_intervalp</parameters>
+				<outputs>x, y, z</outputs>
+				<start_time>0.0</start_time>
+				<stop_time>50.0</stop_time>
+				<output_interval>0.5</output_interval>
+			</settingsFMU>
+		</ExternalModel>
+	</Models>
 """
 
 import numpy as np
@@ -25,9 +44,18 @@ from fmpy import simulate_fmu
 
 reservedNames = ['start_time', 'stop_time', 'output_interval']
 
+def without_keys(d, keys):
+    '''
+    Return a copy of dictionary 'd' without the specified 'keys'.
+    '''
+    return {x: d[x] for x in d if x not in keys}
+
+
 ##### RAVEN methods #####
 def _readMoreXML(raven,xmlNode):
-
+    '''
+    Initialization section. Only run once.
+    '''
     settings = {'filename':'',
                 'start_time':None,'stop_time':None,'output_interval':None,
                 'parameters':{},'outputs':[]}
@@ -71,36 +99,22 @@ def _readMoreXML(raven,xmlNode):
     
     
 def run(raven, Input):
-
+    '''
+    RAVEN recongnizes and runs this section for each run.
+    '''
     # Load input
     parentkey = 'parameters'
-    toRemove = []
+
     for key in raven.settings[parentkey].keys():
-        if key in reservedNames:
-            # Handle scenario where the setup variables are sampled
-            raven.settings[key] = Input[key]
-            toRemove.append(key)
-        else:
-            # Set values from sampled variables
-            raven.settings[parentkey][key] = Input[key]
+        raven.settings[parentkey][key] = Input[key]
     
-    # Remove setup variables from parameters variable
-    for key in toRemove:
-        raven.settings[parentkey].pop(key)
-        
     # Setup
     filename = raven.settings['filename']
     start_time = raven.settings['start_time']
     stop_time = raven.settings['stop_time']
     output_interval = raven.settings['output_interval']
-    parameters = raven.settings['parameters']
+    parameters = without_keys(raven.settings['parameters'], reservedNames)
     outputs = raven.settings['outputs']
-    print('##############')
-    print(raven.settings)
-    print('##############')
-    # print('start_time = {}'.format(start_time))
-    # print('stop_time = {}'.format(stop_time))
-    # print('output_interval = {}'.format(output_interval))
     
     # Simulate
     results = simulate_fmu(filename,
